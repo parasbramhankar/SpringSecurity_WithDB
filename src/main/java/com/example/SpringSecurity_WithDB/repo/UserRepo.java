99527ffa0080a9b26f1abce72d0c4a99feff8602
parasbramhankar <parasbramhankar.in@gmail.com>
2026-02-03 16:04:45 +0530

    Commit

---
+++ src/main/java/com/example/SpringSecurity_WithDB/repo/UserRepo.java
@@ -0,0 +1,10 @@
+package com.example.SpringSecurity_WithDB.repo;
+
+import com.example.SpringSecurity_WithDB.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepo extends JpaRepository<User,Long> {
+    Optional<User>findByUserName(String username);
+}
