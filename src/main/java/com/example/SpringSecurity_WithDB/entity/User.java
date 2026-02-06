02bbb07b272042d10203d870528f83f9544e733b
parasbramhankar <parasbramhankar.in@gmail.com>
2026-02-06 15:24:38 +0530

    Commit

--- src/main/java/com/example/SpringSecurity_WithDB/entity/User.java
+++ src/main/java/com/example/SpringSecurity_WithDB/entity/User.java
@@ -24,7 +24,7 @@ public class User {
     @JoinTable(
             name = "user_roles",
             joinColumns = @JoinColumn(name="user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role-id")
+            inverseJoinColumns = @JoinColumn(name = "role_id")
     )
     private Set<Role>roles;
 }
